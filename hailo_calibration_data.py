1088fb511a896f87f19002141690508b69a10291
LukeDitria <luke.ditria@monash.edu>
2025-03-17 12:23:21 +1100

    create new data dir

--- hailo_calibration_data.py
+++ hailo_calibration_data.py
@@ -52,7 +52,7 @@ def main():
     args = parse_arguments()
 
     # Ensure target directory exists
-    target_dir_path = args.target_dir
+    target_dir_path = os.path.join(args.target_dir, "calib")
     os.makedirs(target_dir_path, exist_ok=True)
 
     # Find all image files in the data directory
